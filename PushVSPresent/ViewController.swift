f32e2b2e8c40d9d9a9af44898ce514b49f619a6e
ThangTruong <thangtm6@fsoft.com.vn>
2017-03-23 17:04:44 +0700

    Phân biệt Push / Present --- Pop / Dismiss

--- PushVSPresent/ViewController.swift
+++ PushVSPresent/ViewController.swift
@@ -19,7 +19,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+class LastViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func pop() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func onClickDismiss() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
